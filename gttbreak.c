1394a25bbb98b3ff07bc8d6961858bddbad91ff2
Ron Minnich <rminnich@gmail.com>
2012-04-12 01:18:41 -0700

    We can now set and control the gtts.
    It seems we can run the graphics from user mode.

---
+++ gttbreak.c
@@ -0,0 +1,23 @@
+#include "video.h"
+
+int cangencode = 0;
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int num = 32768;
+
+	init(argc, argv);
+
+	for(i = 0; i < num; i += 4){
+		u32 word = io_I915_READ32(i|1);
+		u32 base = word;
+		u32 lowbits = word;
+		fprintf(stderr, "%d: [%#x]%#x, %s, %s, %s, %s\n", i, word, base, 
+			lowbits & 8 ? "GFDT": "~GFDT", 
+			lowbits & 4 ? ";Cached in LLC": ";from gttentry", 
+			lowbits & 2 ? ";cacheable L3": "~;cacheable in L3", 
+			lowbits & 1 ? ";V": ";~V");
+		io_I915_WRITE32(i|1,0);
+	}
+}
---
+++ gttset.c
@@ -0,0 +1,33 @@
+#include "video.h"
+
+int cangencode = 0;
+extern u32 gsmphys;
+extern u32 aperture, aperturesize;
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int num = 32768;
+
+	init(argc, argv);
+
+	for(i = 0; i < 32; i += 4){
+		u32 word = io_I915_READ32(i|1);
+		u32 base = word;
+		u32 lowbits = word;
+		fprintf(stderr, "%d: [%#x]%#x, %s, %s, %s, %s\n", i, word, base, 
+			lowbits & 8 ? "GFDT": "~GFDT", 
+			lowbits & 4 ? ";Cached in LLC": ";from gttentry", 
+			lowbits & 2 ? ";cacheable L3": "~;cacheable in L3", 
+			lowbits & 1 ? ";V": ";~V");
+	}
+
+	fprintf(stderr, "# PTEs is %d\n", aperturesize / 4096);
+	fprintf(stderr, "Start of graphics pages would be %#p\n", gsmphys + 2*1024*1024);
+	for(i = 0; i < aperturesize/1024; i += 4){
+		u32 word = gsmphys + 2*1024*1024 + i*4096;
+		io_I915_WRITE32(i|1,word);
+	}
+
+	
+}
