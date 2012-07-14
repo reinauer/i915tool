b60471a0614a589beba132d517d5cf4515a04fb8
Ron Minnich <rminnich@gmail.com>
2012-07-13 17:56:15 -0700

    Fill all 32m
    Signed-off-by: Ron Minnich <rminnich@gmail.com>

--- dumpscreen.c
+++ dumpscreen.c
@@ -12,8 +12,10 @@ int main(int argc, char *argv[])
 
 	print_hex_dump_bytes("", DUMP_PREFIX_ADDRESS, gfx, 256);
 
-	for(i = 0; i < 1048576; i++)
-		gfx[i] = i;
+	/* we do 32M in coreboot */
+	unsigned long long *x = (unsigned long long *)gfx;
+	for(i = 0; i < 32*1024*1024/sizeof(unsigned long long); i += sizeof(unsigned long long))
+		x[i] = i;
 	print_hex_dump_bytes("", DUMP_PREFIX_ADDRESS, gfx, 256);
 
 }
